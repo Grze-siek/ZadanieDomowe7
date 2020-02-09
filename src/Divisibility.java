7e84f15393d987d7fcd0f225451b2b65ea6ab5cf
Grzegorz Ryczkowski <ryczkowski.grzegorz@gmail.com>
2020-02-09 20:42:08 +0100

    Poprawka

--- src/Divisibility.java
+++ src/Divisibility.java
@@ -7,13 +7,15 @@ public class Divisibility {
     {
         int dividedNumber;
         int a = 0;
-        for(dividedNumber = 1; a < 20; dividedNumber++) {
+        for(dividedNumber = 1; a < number; dividedNumber++) {
           a = 0;
             for (int divider = 1; divider <= number; divider++) {
-                if (dividedNumber % divider == 0)
+                if (dividedNumber % divider != 0)
+                    break;
+                else
                     a++;
             }
         }
-        System.out.println(dividedNumber);
+        System.out.println(dividedNumber - 1);
     }
 }
